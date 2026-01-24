36f4718c2bad9a9bd0a4b12822a29e36a9dd80ba
HaoJun Li <lhj15733527022@gmail.com>
2026-01-24 14:20:46 -0500

    week3 first30min
    create a new variable for sub sfm and make an actiontask to let the player can havea random condition for sunFSM

---
+++ Assets/Scripts/AT_GetTool.cs
@@ -0,0 +1,57 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class AT_GetTool : ActionTask {
+
+		public BBParameter<bool> havetool;
+
+		//Use for initialization. This is called only once in the lifetime of the task.
+		//Return null if init was successfull. Return an error string otherwise
+		protected override string OnInit() {
+			return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+
+            float check = UnityEngine.Random.Range(1, 10);
+
+            if (check < 5)
+            {
+                havetool.value = true;
+            }
+            else
+            {
+                havetool.value = false;
+            }
+
+			Debug.Log(havetool);
+
+
+        }
+
+        //Called once per frame while the action is active.
+        protected override void OnUpdate() {
+			
+
+
+
+		}
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
